24f621f371cc79d88f3e2eb1b36cf83b5afb20af
jeonhyerin <marie0902@naver.com>
2016-06-19 15:24:48 +0000

    맛집 공유 게시판 글 삭제 파일
    맛집 공유 게시판 글 삭제/ 디비연동 파일입니다

---
+++ erase.php
@@ -0,0 +1,28 @@
+﻿<html>
+<head>
+<title> 맛집 공유 게시판 </title>
+<link rel="stylesheet" href="./css/style.css">
+</head>
+<body topmargin=0 leftmargin=0 text=#464646>
+<center>
+<br>
+<form action="erase_plus.php?num=<?=$_GET['num']?> method="post">
+<table width="300" border=0 cellpadding=2 cellspacing=1 bgcolor=#777777>
+	<tr>
+		<td height=20  align= center bgcolor=#999999>
+			<font color=white><b> 비밀번호 확인</b></font>
+		</td>
+	</tr>
+	<tr>
+		<td align=center>
+			<font color=white><b> 비밀번호: </b>
+			<input type="password" name="password" size="8">
+			<input type="submit" value="확인">
+			<input type="button" value="취소" onclick="history.back(-1)">
+		</td>
+	</tr>
+</table>
+</form>
+</center>
+</body>
+</html>
---
+++ erase_plus.php
@@ -0,0 +1,30 @@
+﻿<?php
+	include "dbconn.php";
+	
+	$num = $_GET['num'];
+	$password = $_POST['password'];
+
+	$sql = "SELECT password FROM free WHERE num=".$num;
+	$result = mysqli_query($connect, $sql);
+	$row = mysqli_fetch_assoc($result);
+
+	if($password == $row['password'])
+	{
+		$sql = "DELETE FROM free WHERE num=".$num;
+		$result = mysqli_query($connect, $sql);
+	}
+
+	else 
+	{
+		echo("
+		<script>
+		alert('비밀번호가 틀립니다.');
+		history.go(-1);
+		</script>
+		");
+		exit;
+	}
+?>
+<center>
+<meta http-equiv='Refresh' content='1; URL=list1.php'>
+<FONT size=2> 삭제되었습니다. </FONT>
