2e1bd5e749860cf3a95fcc9097c54d09ececf512
jeonhyerin <marie0902@naver.com>
2016-06-19 15:18:25 +0000

    로그아웃 php
    로그아웃을 위한 php파일입니다

---
+++ logout.php
@@ -0,0 +1,18 @@
+﻿<html>
+<head>
+</head>
+<body>
+<?
+	session_start();
+	$logout = $_SESSION["id"];
+	if($_SESSION["id"]!=NULL)
+	{
+		session_destroy();
+	}
+?>
+	<form action="login.php" method="POST" align=center>
+		<h1>로그아웃 되었습니다.</h1>
+		<input type=submit value="Login">
+	</form>
+</body>
+</html>
