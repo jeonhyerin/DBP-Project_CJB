7d81f8b0f1ea64685013b5c689bf83ef7d4cad11
jeonhyerin <marie0902@naver.com>
2016-06-22 04:58:15 +0000

    디비 연동 php
    디비에 연결하기 위해 코드마다 include해서 썼던 php파일입니다.

---
+++ dbconn.php
@@ -0,0 +1,10 @@
+﻿<?php
+	$connect = mysqli_connect("localhost", "root", "autoset", "delicious");
+
+	if(!$connect) {
+		echo "Error: Unable to connect to MySQL." . PHP_EOL;
+		exit;
+	}
+
+	mysqli_set_charset($connect, 'euc-kr');
+?>
