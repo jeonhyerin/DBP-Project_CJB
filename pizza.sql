f514042639f975766a68645cb1491a88975f81c0
jeonhyerin <marie0902@naver.com>
2016-06-22 04:24:14 +0000

    맛집 메뉴 게시판 관련 sql입니다.
    맛집 메뉴 치킨, 중국음식, 프랜차이즈, 일본음식, 족발, 한국음식, 야식, 피자 메뉴 게시판의 sql입니다.

---
+++ pizza.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE pizza(
+num int not null auto_increment,
+name char(15) not null,
+password char(100) not null,
+subject char(100) not null,
+content text not null,
+regist_day datetime not null,
+hit int,
+file_name_0 char(40),
+file_name_1 char(40),
+file_name_2 char(40),
+file_name_3 char(40),
+file_name_4 char(40),
+file_copied_0 char(30),
+file_copied_1 char(30),
+file_copied_2 char(30),
+file_copied_3 char(30),
+file_copied_4 char(30),
+primary key(num)
+);
